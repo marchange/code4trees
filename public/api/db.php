d5e29308b83a69139dd46cc2471df35fee3332ed
marchange <byemychill@gmail.com>
2026-07-17 23:06:24 +0200

    testing :D

--- public/api/db.php
+++ public/api/db.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
+
 $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 
 $host     = $_ENV['DB_HOST'];
--- public/api/get_setup_data.php
+++ public/api/get_setup_data.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 // api/get_setup_data.php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
